9c963bc5b0159119e5db28af4b3d1e5acab135c6
adhammhaydat <adhammohidat123@gmail.com>
2021-10-17 22:36:02 +0300

    test: add after successfully

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -142,18 +142,18 @@ def test_linked_list_insert_before_one():
   for i in range(len(actual)):
       assert actual[i] == expected
 
-# def test_linked_list_insert_after_one():
-#   expected =[1,2,4,3]
-#   ll = LinkedList()
+def test_linked_list_insert_after_one():
+  expected =[1,2,4,3]
+  ll = LinkedList()
 
-#   # Act
-#   ll.append(1)
-#   ll.append(2)
-#   ll.append(3)
-#   ll.insert_before(3,4)
-#   actual=[]
-#   actual.append(ll.to_string())
+  # Act
+  ll.append(1)
+  ll.append(2)
+  ll.append(3)
+  ll.insert_before(3,4)
+  actual=[]
+  actual.append(ll.to_string())
 
-#   # Assert
-#   for i in range(len(actual)):
-#       assert actual[i] == expected
+  # Assert
+  for i in range(len(actual)):
+      assert actual[i] == expected
