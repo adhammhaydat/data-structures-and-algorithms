3ac30b27ba8838ada44a759aef80adb29dbc7d0d
adhammhaydat <adhammohidat123@gmail.com>
2021-10-31 23:02:46 +0200

    add readme file

--- python/linked_list/tree.py
+++ python/linked_list/tree.py
@@ -146,18 +146,4 @@ class BinaryTree:
                 max_number=list_of_items[i]
         return max_number
 
-tree = BinaryTree()
 
-  # Create Nodes for 1,2,3,4
-# a_node = Node(1)
-# b_node = Node(212)
-# c_node = Node(5)
-# d_node = Node(4)
-# a_node.left = b_node
-# a_node.right = c_node
-# b_node.left = d_node
-
-  # Add Root node to tree
-# tree.root=a_node
-tree.root = None
-print(tree.find_maximum_value())
